53dd0f12a444a1016dc6ea5e6f31f1acae02ab41
Modesto Figuereo Fernandez <figuereo.fernandez.modesto@gmail.com>
2016-04-21 16:57:25 -0400

    Add changes to negator.js file. Change name of test/negate.test.js to test/negator.test.js and add changes to it.

--- negator.js
+++ negator.js
@@ -7,7 +7,11 @@
     global[name] = definition();
   }
 })('negator', this, function () {
-  function negator (context, fn) {
+  function negator(context, fn) {
+    if (!arguments.length) {
+      return Object.create(negatorBuilderProto);
+    }
+
     var args = [].slice.apply(arguments, [2]);
     if (!context || typeof context === 'function') {
       args.unshift(fn);
@@ -15,11 +19,33 @@
     }
 
     return function () {
-      var moreArgs = [].slice.apply(arguments, [1]);
+      var moreArgs = [].slice.apply(arguments, [0]);
 
       return !fn.apply(context || {}, args.concat(moreArgs));
     }
   }
+
+  var negatorBuilderProto = {
+    __arguments: [],
+    __context: null,
+    __expression: null,
+    expression: function expression(fn) {
+      this.__expression = fn;
+      return this;
+    },
+    context: function context(ctx) {
+      this.__context = ctx;
+      return this;
+    },
+    argument: function argument(arg) {
+      this.__arguments.push(arg);
+      return this;
+    },
+    build: function build() {
+      var args = [this.__context, this.__expression].concat(this.__arguments);
+      return negator.apply({}, args);
+    },
+  };
 
   return negator;
 });
---
+++ test/negator.test.js
@@ -0,0 +1,149 @@
+var tape = require('tape'),
+    before = tape,
+    after = tape,
+    test = beforeEach(tape, function (assert) {
+      assert.end();
+    }),
+    test = afterEach(test, function (assert) {
+      assert.end();
+    })
+    negator = require('../negator');
+
+test('negator() simple use', function (assert) {
+  var f = negator(function () { return true });
+  var t = negator(function () { return false });
+
+  assert.false(f(), 'f() should return false');
+  assert.true(t(), 'v() should return true');
+  assert.end();
+});
+
+test('negator() simple use passing arguments', function (assert) {
+  function isUnderAge(age) { return age < 18 }
+
+  var isNotUnderAge = negator(isUnderAge, 25);
+
+  assert.true(isNotUnderAge(), 'isNotUnderAge() should return true');
+  assert.end();
+});
+
+test('negator() simple use passing context', function (assert) {
+  var isNotUnderAge = negator(john, function () { return this.isUnderAge(); });
+
+  assert.false(isNotUnderAge(), 'isNotUnderAge() should return false');
+  assert.end();
+});
+
+test('negator() simple use passing context and arguments', function (assert) {
+  var minAge = 18;
+  var maxAge = 30;
+  var isNotAgeInRange = negator(
+    john,
+    function (minAge, maxAge) {
+      return this.age >= minAge && this.age <= maxAge;
+    },
+    minAge
+    // we could've passed maxAge too,
+    // but we didn't just to demonstrate that can pass it latter
+  );
+
+  assert.true(isNotAgeInRange(maxAge), 'isAgeInRange() should return true');
+  assert.end();
+});
+
+test('negator() use as a builder', function (assert) {
+  var isNotAgeInRange = negator()
+    .expression(function (minAge, maxAge) {
+      return this.age >= minAge && this.age <= maxAge;
+    })
+    .context(john)
+    .argument(18)
+    .argument(30)
+    .build();
+
+  assert.true(isNotAgeInRange(), 'isAgeInRange() should return true');
+  assert.end();
+});
+
+test('negator() use as a builder with a collection of people', function (assert) {
+  var minAge = 18;
+  var maxAge = 30;
+  var isNotAgeInRangeBuilder = negator()
+    .expression(function (minAge, maxAge) {
+      return this.age >= minAge && this.age <= maxAge;
+    })
+    .argument(minAge)
+    .argument(maxAge);
+
+  for (var person in people) {
+    var isNotAgeInRange = isNotAgeInRangeBuilder
+      .context(people[person])
+      .build();
+
+    if (people[person].age >= minAge && people[person].age <= maxAge) {
+      assert.false(isNotAgeInRange(), 'isAgeInRange() should return false for ' + people[person].firtName);
+    } else {
+      assert.true(isNotAgeInRange(), 'isAgeInRange() should return true for ' + people[person].firtName);
+    }
+  }
+
+  assert.end();
+});
+
+function beforeEach(t, handler) {
+  return function tapish(name, listener) {
+    t(name, function (assert) {
+      var _end = assert.end;
+      assert.end = function () {
+        assert.end = _end;
+        listener(assert);
+      };
+
+      handler(assert);
+    });
+  };
+}
+
+function afterEach(t, handler) {
+  return function tapish(name, listener) {
+    t(name, function (assert) {
+      var _end = assert.end;
+      assert.end = function () {
+        assert.end = _end;
+        handler(assert);
+      };
+
+      listener(assert);
+    });
+  };
+}
+
+var john = {
+  firtName: 'John',
+  lastName: 'Connor',
+  age: 17,
+  isUnderAge: function isUnderAge() { return this.age < 18 }
+};
+
+var people = [
+  {
+    firtName: 'Francis',
+    lastName: 'Brito',
+    age: 21,
+  },
+  {
+    firtName: 'Michael',
+    lastName: 'Castro',
+    age: 22,
+  },
+  {
+    firtName: 'Modesto',
+    lastName: 'Figuereo',
+    age: 22,
+  },
+  {
+    firtName: 'Onil',
+    lastName: 'Pereyra',
+    age: 36,
+  },
+]
